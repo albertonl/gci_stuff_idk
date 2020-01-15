2236b86a8fd44a4de8f20e382df885c319f8e715
Alberto Navalon Lillo <alberto.navalonlillo@gmail.com>
2020-01-15 08:55:34 +0100

    add scraper

--- apertium/bidix/eng-spa/scraper.py
+++ apertium/bidix/eng-spa/scraper.py
@@ -1,32 +1,54 @@
 import requests
+import progressbar
+import time
 from bs4 import BeautifulSoup
 
-URL = 'https://dictionary.cambridge.org/spellcheck/spanish-english/?q='
-url2 = ''
+URL = "https://dictionary.cambridge.org/spellcheck/spanish-english/?q="
+url2 = ""
 page = requests.get(URL)
 
 unknown = False
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-with open('drae.eng.txt') as f:
+with open("drae.eng.txt") as f:
 	lines = [line.rstrip() for line in f]
-
-for i in range(len(lines)):
-	if lines[i][0]=='*':
-		url2  = URL
-		for j in range(len(lines[i])):
-			if j!=0:
-				url2 += lines[i][j]
+with progressbar.ProgressBar(max_value=progressbar.UnknownLength) as bar:
+	for i in range(len(lines)):
+		bar.update(i)
+		try:
+			if lines[i][0]=='*':
+				url2 = URL
+				for j in range(len(lines[i])):
+					if j!=0:
+						url2 += lines[i][j]
+				print(url2)
 				page = requests.get(url2)
 
-				soup = BeautifulSoup(page.content, 'html.parser')
+				soup = BeautifulSoup(page.content, "html.parser")
 
-				msg = results.find_all('section', class_='lpb-10 lbb')
+				msg = soup.find_all("section", class_="lpb-10 lbb")
+				
+				if "Search suggestions for" in str(page.content):
+					unknown = True
+					print("URL: ", url2, " (STATUS: known)")
+					print("Ignoring...")
+				elif unknown==False:
+						with open("unknownlinks.txt", "a") as f2:
+							f2.write(url2 + "\n")
+							print("URL: ", url2, " (STATUS: unknown)")
+							print("Writing to file...")
+				"""
 				for msg_single in msg:
-					if msg_single == 'Your search terms did not match any definitions.':
+					if msg_single == "Your search terms did not match any definitions." or "Search suggestions for" in msg_single:
 						unknown = True
-
-				if unknown==False:
-					with open("unknownlinks.txt", "a") as f2:
-						f2.write(url2)
+						print("URL: ", url2, " (STATUS: known)")
+						print("Ignoring...")
+					if unknown==False:
+						with open("unknownlinks.txt", "a") as f2:
+							f2.write(url2, "\n")
+							print("URL: ", url2, " (STATUS: unknown)")
+							print("Writing to file...")
+			"""
+		except IndexError:
+			continue
